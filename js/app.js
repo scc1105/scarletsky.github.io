c0a31e7fe94ce3c8262ff8fa6c9dae39a6ea826e
scarletsky <scarletsky1025@gmail.com>
2016-04-20 17:17:41 +0800

    Site updated: 2016-04-20 17:17:40

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 (function() {
   'use strict';
 
@@ -5,73 +6,196 @@
   var $body = document.body;
   var $toc = document.getElementById('toc');
   var $backTop = document.getElementById('backTop');
+  var $toolboxMobile = document.getElementById('toolbox-mobile');
+  var $cover = document.getElementById('cover');
+  var $close = document.getElementById('close');
+  var $modalDialog = document.getElementById('modal-dialog');
   var scrollTop = 0;
 
+
+  (function init() {
+    if ($backTop) {
+      $body.scrollTop > 10 ? Util.addClass($backTop, 'show') : Util.removeClass($backTop, 'show');
+    }
+
+    if ($toc) {
+      $body.scrollTop > 180 ? Util.addClass($toc, 'fixed') : Util.removeClass($toc, 'fixed');
+    }
+
+  }());
+
   document.addEventListener('DOMContentLoaded', function() {
-      FastClick.attach(document.body);
+    FastClick.attach(document.body);
   }, false);
 
+  window.noZensmooth = true;
+
+  // scroll spy
+  scrollSpy.init({
+    nodeList: document.querySelectorAll('.toc-link'),
+    scrollTarget: window
+  });
+
   // toc and backTop
-  bind(window, 'scroll', function () {
+  Util.bind(window, 'scroll', function() {
     scrollTop = $body.scrollTop;
     if ($toc) {
-      scrollTop > 200 ? addClass($toc, 'fixed') : removeClass($toc, 'fixed');
+      scrollTop > 180 ? Util.addClass($toc, 'fixed') : Util.removeClass($toc, 'fixed');
     }
 
     if ($backTop) {
-      scrollTop > 10 ? addClass($backTop, 'show') : removeClass($backTop, 'show');
+      scrollTop > 10 ? Util.addClass($backTop, 'show') : Util.removeClass($backTop, 'show');
     }
   });
 
-  if ($toc) {
-    bind($backTop, 'click', function () {
-      scroll('0', 400);
+  if ($backTop) {
+    Util.bind($backTop, 'click', function() {
+      zenscroll.to($body)
     });
   }
 
+  if ($toc) {
+    var $toc = document.getElementById('toc');
+    var $tocLinks = document.querySelectorAll('.toc-link');
+    var links = Array.prototype.slice.call($tocLinks);
+
+    links.forEach(function(element) {
+      Util.bind(element, 'click', function(e) {
+        var $target = document.getElementById(this.hash.substring(1));
+        zenscroll.to($target)
+        e.preventDefault();
+      });
+    });
+  }
+
+  if ($toolboxMobile) {
+    Util.bind($toolboxMobile, 'click', function() {
+      Util.addClass($modalDialog, 'show-dialog')
+      Util.removeClass($modalDialog, 'hide-dialog');
+
+      Util.addClass($cover, 'show')
+      Util.removeClass($cover, 'hide');
+    });
+
+
+    Util.bind($cover, 'click', closeModal);
+    Util.bind($close, 'click', closeModal);
+  }
+
+
+  if (location.pathname === '/search/') {
+    Util.request('GET', '/search.json', function(data) {
+      var $inputSearch = document.getElementById('input-search');
+      Util.bind($inputSearch, 'keyup', function() {
+        var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
+
+        if (this.value.trim().length <= 0) {
+          return;
+        }
+
+        var results = filterPosts(data, keywords);
+        var $listSearch = document.getElementById('list-search');
+        $listSearch.innerHTML = createInnerHTML(results);
+      });
+
+    });
+  }
+
+
   ///////////////////
-  function scroll(scrollTo, time) {
-      var scrollFrom = parseInt(document.body.scrollTop),
-          i = 0,
-          runEvery = 5; // run every 5ms
 
-      scrollTo = parseInt(scrollTo);
-      time /= runEvery;
+  function filterPosts(data, keywords) {
+    var results = [];
 
-      var interval = setInterval(function () {
-          i++;
+    data.forEach(function(item) {
+      var isMatch = false;
+      var matchKeyWords = [];
+      item.content = item.content.replace(/<[^>]*>/g, '');
 
-          document.body.scrollTop = (scrollTo - scrollFrom) / time * i + scrollFrom;
+      keywords.forEach(function(word) {
+        var reg = new RegExp(word, 'i');
+        var indexTitle = item.title.search(reg);
+        var indexContent = item.content.search(reg);
 
-          if (i >= time) {
-              clearInterval(interval);
-          }
-      }, runEvery);
+        if (indexTitle > -1 || indexContent > -1) {
+          isMatch = true;
+          matchKeyWords.push(word);
+        }
+      });
+
+      if (isMatch) {
+        item.matchKeyWords = matchKeyWords;
+        results.push(item);
+      }
+    });
+
+    return results;
   }
 
-  function bind(element, name, listener) {
-    element.addEventListener(name, listener, false);
+  function createInnerHTML(results) {
+    var content = '';
+    results.forEach(function(item) {
+      var postContent;
+      postContent = highlightText(item.content, item.matchKeyWords);
+      postContent = getPreviewContent(postContent, item.matchKeyWords);
+
+      item.title = highlightText(item.title, item.matchKeyWords);
+
+      item = '<li class="item">' +
+        '<a href="' + item.url + '"" target="_blank">' +
+        '<h3 class="title">' + item.title + '</h3>' +
+        '</a>' +
+        '<p class="post-content">' + postContent + '</h3>' +
+        '</li>';
+      content += item;
+    });
+
+    return content;
   }
 
- function addClass(element, className) {
-    var classes = element.className ? element.className.split(' ') : [];
-    if (classes.indexOf(className) < 0) {
-      classes.push(className);
+  function getPreviewContent(content, matchKeyWords) {
+    var isMatch = false;
+    var index = 0;
+    matchKeyWords.forEach(function(word) {
+      var reg = new RegExp(word, 'i');
+      index = content.search(reg);
+      if (index < 0) {
+        return;
+      }
+
+      isMatch = true;
+    });
+
+    if (isMatch) {
+      if (index < 120) {
+        content = content.substr(0, 140);
+      } else {
+        content = content.substr(index - 60, 200);
+      }
+    } else {
+      content = content.substr(0, 120);
     }
 
-    element.className = classes.join(' ');
-    return element;
+    return content;
   }
 
-  function removeClass(element, className) {
-    var classes = element.className ? element.className.split(' ') : [];
-    var index = classes.indexOf(className);
-    if (index > -1) {
-      classes.splice(index, 1);
-    }
+  function highlightText(text, matchKeyWords) {
+    text = text.replace(/<[^>]*>/g, '');
+    matchKeyWords.forEach(function(word) {
+      var reg = new RegExp('(' + word + ')', 'ig');
+      text = text.replace(reg, '<span class="color-hightlight">$1</span>');
+    });
 
-    element.className = classes.join(' ');
-    return element;
+    return text;
   }
+
+
+  function closeModal() {
+    Util.addClass($modalDialog, 'hide-dialog')
+    Util.removeClass($modalDialog, 'show-dialog');
+    Util.addClass($cover, 'hide')
+    Util.removeClass($cover, 'show');
+  }
+
 
 }());
